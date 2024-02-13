475f87641b221a5775e4a8bddfd70a09f83bb321
Solomonraja-datasirpi <solomonraja.t@datasirpi.com>
2024-02-13 11:12:14 +0530

    new

--- bin/demo.dart
+++ bin/demo.dart
@@ -1,14 +1,14 @@
 
-//async and await
-main() async{
-  print('Line1');
-  await display();
-  print('Line3');
-}
-Future<void> display() async{
-  Future.delayed(Duration(seconds: 5),()=>print('Line2'));
+class Students{
+  static var name='solomon';  //static variable
+
+  static test(){  //static method
+    var a=Students.name;
+    print('welcome $a');
+  }
 }
 
-//sync will wait for the data and execute
-//async will not wait for the data and complete the execution 1st
-//for async consists two class, i.e future and steam
+main(){
+  print(Students.name);
+  Students.test();
+}
